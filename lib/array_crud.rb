8c24be1fa192788f3d0c9239478a4cb770aaa02f
Cathy D'Onofrio <catd825@gmail.com>
2020-03-14 19:40:58 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -15,7 +15,7 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-   puts add_element_to_start_of_array.unshift
+   ["wow", "I", "am", "really", "learning","arrays!"].unshift
 end
 
 def remove_element_from_start_of_array(array)
