7982b5c23de15988fb34e98a28015d087b17d580
Cathy D'Onofrio <catd825@gmail.com>
2020-03-14 19:38:28 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,7 +7,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  puts ["wow", "I", "am", "really", "learning"].push("arrays!")
+  ["wow", "I", "am", "really", "learning"].push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
