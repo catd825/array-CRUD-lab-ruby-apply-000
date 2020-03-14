ad45147b1228b338248442ccdf3b496e08ba73d3
Cathy D'Onofrio <catd825@gmail.com>
2020-03-14 19:31:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -7,23 +7,23 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  create_an_array.push("arrays!")
+  puts create_an_array.push("arrays!")
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_end_of_array.unshift("wow")
+  puts add_element_to_end_of_array.unshift("wow")
 end
 
 def remove_element_from_end_of_array(array)
-  add_element_to_start_of_array.pop
+   puts add_element_to_start_of_array.unshift
 end
 
 def remove_element_from_start_of_array(array)
-
+   puts remove_element_from_end_of_array.pop
 end
 
 def retrieve_element_from_index(array, index_number)
-
+  puts remove_element_from_start_of_array.2
 end
 
 def retrieve_first_element_from_array(array)
