3f88a8444e21dc3f001ba86dab9bb726a5deb635
Cathy D'Onofrio <catd825@gmail.com>
2020-03-14 19:45:18 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,13 +23,13 @@ def remove_element_from_start_of_array(array)
 end
 
 def retrieve_element_from_index(array, index_number)
-  puts remove_element_from_start_of_array[2]
+  ["I", "am", "really", "learning"][2]
 end
 
 def retrieve_first_element_from_array(array)
-  puts retrieve_first_element_from_array[0]
+  ["I", "am", "really", "learning"][0]
 end
 
 def retrieve_last_element_from_array(array)
-  puts retrieve_first_element_from_array[-1]
+    ["I", "am", "really", "learning"][-1]
 end
